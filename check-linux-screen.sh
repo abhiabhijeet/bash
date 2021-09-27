ec054f8f5ec14a050156055ff3a8d81edb34fc38
Abhijeet <88643508+devops-abhijeet@users.noreply.github.com>
2021-09-27 11:40:13 +0530

    Update check-linux-screen.sh

--- check-linux-screen.sh
+++ check-linux-screen.sh
@@ -1,27 +1,20 @@
 #!/bin/sh
 
-function send_pager(){
-	ip="$1"
-	#pd-send -k "$PAGER_KEY" -t trigger -d "Write Message Here" -i `date +%s`
-}
 ifExists=$(ls /var/run/screen/S-ubuntu/ | grep -w "Screen_name")
 if [[ -z "$ifExists" ]];then
 
-	echo "[ `date` ] Screen does not exist,sending pager alert."
-	ssh ubuntu@$MachineIP "$(typeset -f);send_pager `hostname -I`"
+	echo "[ `date` ] Screen does not exist"
 else
 	echo "[ `date` ] Screen found."
 fi
-function pager(){
-	ip="$1"
-	pd-send -k "$PAGER_KEY" -t trigger -d "Write Message Here" -i `date +%s`
-}
+
+#Check for running process
+
 pid=$(ps -ef | grep "$STRING_NAME" | grep -vw "grep" | awk '{print $2}')
 
 if [ -z "$pid" ];
 then
 	echo "Script not running Please check imediately"
-	ssh ubuntu@$MachineIP "$(typeset -f);pager `hostname -i`"
 else
 	echo "Script is Runnning fine `hostname -i`"
 fi
