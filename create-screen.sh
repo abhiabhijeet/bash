f67c9495c5806ab8b73531783d2ce8770f74fdd5
Abhijeet <88643508+devops-abhijeet@users.noreply.github.com>
2021-09-27 12:37:14 +0530

    Update create-screen.sh

--- create-screen.sh
+++ create-screen.sh
@@ -5,11 +5,12 @@ scr_name="test"
 pid=`screen -ls | grep -w $scr_name`
 if [ -z "$pid" ];
 then
+	echo "Creating $scr_name and Running command to Launch UI"
 	#screen -dmS $scr_name
 	screen -dmS $scr_name  sh
 	screen -S $scr_name -X stuff "cd /home/ubuntu
 	"
-	screen -S $scr_name -X stuff "bash un_script.sh
+	screen -S $scr_name -X stuff "bash run_script.sh
 	"
 else
 	echo "found $scr_name"
